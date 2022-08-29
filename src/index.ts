8bffd296e8c4bd57e1c9e0c40fd4f62ca233259c
Stephen Vector <55390907+stephenvector@users.noreply.github.com>
2022-08-28 23:59:30 -0400

    setup sentry

--- src/index.ts
+++ src/index.ts
@@ -1,3 +1,16 @@
+import * as Sentry from "@sentry/browser";
+import { BrowserTracing } from "@sentry/tracing";
+
+Sentry.init({
+  dsn: "https://01e3fd1915cd49bab2c8902e86b5750b@o64982.ingest.sentry.io/6697134",
+  integrations: [new BrowserTracing()],
+
+  // Set tracesSampleRate to 1.0 to capture 100%
+  // of transactions for performance monitoring.
+  // We recommend adjusting this value in production
+  tracesSampleRate: 1.0,
+});
+
 import WebcamApp from "./WebcamApp";
 import "./styles.css";
 
